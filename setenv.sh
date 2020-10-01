194f9bcb7ff06a247710ec39adcb15a3b8dca8dc
Guntur <guntutur@gmail.com>
2020-10-01 09:31:43 +0700

    chore(config) : remove default credential in setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export COUCHDB_USER=admin
-export COUCHDB_PWD=iniadmin
-export COUCHDB_HOST=13.250.43.79
-export COUCHDB_PORT=5984
-export COUCHDB_NAME=todos_guntutur
+export COUCHDB_USER=
+export COUCHDB_PWD=
+export COUCHDB_HOST=
+export COUCHDB_PORT=
+export COUCHDB_NAME=
